8ed6150fc55ef3845edd2124555377ad34e284ee
greyyT <nhannguyen8338@gmail.com>
2024-02-06 21:51:04 +0700

    feat(routes): Initialize routes for Express backend

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from 'express'
-import bodyParser from "body-parser";
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+
+import { clientRoutes, generalRoutes, managementRoutes, salesRoutes } from './routes';
 
 // CONFIGURATION
 dotenv.config();
@@ -17,12 +19,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
 // ROUTES
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
+// APP
 app.get('/ping', (req: Request, res: Response) => {
-    res.send("pong");
-})
+  res.send('pong');
+});
 
 app.listen(8080, () => {
-    console.log('Server running at http://localhost:8080')
-})
+  console.log('Server running at http://localhost:8080');
+});
---
+++ server/routes/index.ts
@@ -0,0 +1,6 @@
+import clientRoutes from './client';
+import generalRoutes from './general';
+import managementRoutes from './management';
+import salesRoutes from './sales';
+
+export { clientRoutes, generalRoutes, managementRoutes, salesRoutes };
