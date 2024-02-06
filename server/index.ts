ecadd54e4e1ea080612671ae49c2ac07925b5463
greyyT <nhannguyen8338@gmail.com>
2024-02-06 21:41:05 +0700

    feat(server): initialize setup for Express backend
    - Create basic folder structure
    - Install Express and necessary dependencies
    - Set up initial server file

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express'
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+// ROUTES
+app.get('/ping', (req: Request, res: Response) => {
+    res.send("pong");
+})
+
+app.listen(8080, () => {
+    console.log('Server running at http://localhost:8080')
+})
