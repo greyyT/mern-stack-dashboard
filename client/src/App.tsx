9ccbd16b98b54f2c97eece9576e7931d90f7b655
greyyT <nhannguyen8338@gmail.com>
2024-02-07 15:08:37 +0700

    feat(client): Configure MUI theme for app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
+import { CssBaseline, ThemeOptions, ThemeProvider, createTheme } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+
+import { themeSettings } from './theme';
+
 const App = () => {
+  const mode = useSelector((state: any) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode) as ThemeOptions), [mode]);
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
   );
 };
 
