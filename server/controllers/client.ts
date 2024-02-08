45c8c175f3633c23d1ec93d9be96557c2b84e4ae
greyyT <nhannguyen8338@gmail.com>
2024-02-09 01:19:55 +0700

    feat(server): Add route for retrieving customer list

--- server/controllers/client.ts
+++ server/controllers/client.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Product from '../models/Product';
 import ProductStat from '../models/ProductStat';
+import User from '../models/User';
 
 export const getProducts = async (req: Request, res: Response) => {
   try {
@@ -20,6 +21,16 @@ export const getProducts = async (req: Request, res: Response) => {
     );
 
     res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
+
+export const getCustomers = async (req: Request, res: Response) => {
+  try {
+    const customers = await User.find({ role: 'user' }).select('-password');
+
+    res.status(200).json(customers);
   } catch (error) {
     res.status(404).json({ message: error });
   }
