4b1bca88c8a24d24cbe038d52e4d2a1e26c85f87
greyyT <nhannguyen8338@gmail.com>
2024-02-12 17:15:24 +0700

    feat(server): Create API route for returning list of admins

---
+++ server/controllers/management.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+
+export const getAdmins = async (req: Request, res: Response) => {
+  try {
+    const admins = await User.find({ role: 'admin' }).select('-password');
+    res.status(200).json(admins);
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
+  }
+};
